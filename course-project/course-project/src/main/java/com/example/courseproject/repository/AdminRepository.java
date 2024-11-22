b015c910211406fb93dbf8f7044920e6c9c5e429
Vinayak <msvinayak2@gmail.com>
2024-11-22 23:05:38 +0530

    Delete course-project directory
    Signed-off-by: Vinayak <msvinayak2@gmail.com>

--- course-project/course-project/src/main/java/com/example/courseproject/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.courseproject.repository;
-
-import com.example.courseproject.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<User, Integer> {
-    User findByUsernameAndPassword(String username, String password);
-}
